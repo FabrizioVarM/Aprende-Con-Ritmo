d53900396daaaec1e871843b72030eefbbcd33b2
fabrozio vargas xd <afabriziovm@hotmail.com>
2026-02-07 02:20:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5164110435-e4c80",
+  "appId": "1:741647661492:web:538ba3a412bdc669e4dae1",
+  "apiKey": "AIzaSyCRCgXmUbT3SfrcfGi-dLAi-k9oPWpQ5Tk",
+  "authDomain": "studio-5164110435-e4c80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "741647661492"
+};
