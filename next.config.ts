45405075ca2b2d132a5b9dc857b36858a320c4c8
fabrozio vargas xd <afabriziovm@hotmail.com>
2026-02-12 17:42:56 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://pikaso.

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pikaso.cdnpk.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
