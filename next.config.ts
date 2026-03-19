9941ab0d774e0f486ef5f746f3ffa09c3aecec92
fabrozio vargas xd <afabriziovm@hotmail.com>
2026-03-19 10:56:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -100,6 +100,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'blog.musicopolix.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
